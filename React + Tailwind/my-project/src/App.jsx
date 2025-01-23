3fdf98be4c850545b11e793b2220fe01c40da548
Zain Ali <zainchamp5@gmail.com>
2025-01-23 23:49:08 +0500

    starting project using react

---
+++ React + Tailwind/my-project/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="main bg-black text-white h-screen w-full">
+        <h1>Start Your Project</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
