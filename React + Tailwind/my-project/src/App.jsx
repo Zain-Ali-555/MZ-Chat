5cdf38ab74942562d9dda620e724ce4dadb7a633
Zain Ali <zainchamp5@gmail.com>
2025-01-24 23:09:10 +0500

    make just basic sturcture mean ui

--- React + Tailwind/my-project/src/App.jsx
+++ React + Tailwind/my-project/src/App.jsx
@@ -2,7 +2,15 @@ const App = () => {
   return (
     <>
       <div className="main bg-black text-white h-screen w-full">
-        <h1>Start Your Project</h1>
+        <h1 className="pt-10 text-3xl flex justify-center">MZ Chat</h1>
+        <div className="btns h-[88vh] w-full flex justify-center items-center gap-10">
+          <div className="chatBtn border-2 rounded-2xl p-2 cursor-pointer">
+            <h5>Start Chat</h5>
+          </div>
+          <div className="SendBtn border-2 rounded-2xl p-2 cursor-pointer">
+            <h5>Send File</h5>
+          </div>
+        </div>
       </div>
     </>
   );
